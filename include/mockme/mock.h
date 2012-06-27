c11656aa919755498e14d65bd26a10964c88bd97
Cédric Meuter <cedric.meuter@gmail.com>
2012-06-27 11:09:43 +0200

    cleaned up manually generated double/mock/stub --> it's mockme's job to do that :-)

--- include/mockme/mock.h
+++ include/mockme/mock.h
@@ -6,14 +6,18 @@
 #include <setjmp.h>
 #include <mockme/cmockery.h>
 
-#define INPUT_VALUE(value)
-#define INPUT_MEMORY(ptr, size)
-#define INPUT_STRING(value)
+#if !defined(MOCK_ME)
+
+#define INPUT_VALUE(value)			do { (void)value; } while(0)
+#define INPUT_MEMORY(ptr, size)		do { (void)ptr; (void)size; } while(0)
+#define INPUT_STRING(value)         do { (void)value; } while(0)
 
 #define __FIRST_ARG(x, args...)\
 	x
 
 #define DEFAULT_STUB(args...)\
 	return __FIRST_ARG(args)
+
+#endif
 
 #endif
